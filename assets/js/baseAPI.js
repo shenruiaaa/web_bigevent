e72588d8d826b2d5b7958be314b4d929ca9450ae
sherry <sr2226338600@163.com>
2022-08-11 17:51:52 +0800

    tijiao

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,18 @@
 $.ajaxPrefilter(function (options) {
     //在发起真正的Ajax请求之前，统一拼接的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
+    //统一为有权限的接口设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = { Authorization: localStorage.getItem('token') || '' }
+    }
+    //全局统一挂载complete回调函数
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //1.清除本地存储中的Token
+            localStorage.removeItem('token')
+            //2.重新跳转到登录页
+            location.href = '/login.html'
+
+        }
+    }
 })
